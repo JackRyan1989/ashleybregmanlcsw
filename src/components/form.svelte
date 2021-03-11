caa4e4491ca76c0272395724af0896c44bda8ae4
JackRyan1989 <jack.jackryan@gmail.com>
2021-03-11 11:09:41 -0500

    send to home

--- src/components/form.svelte
+++ src/components/form.svelte
@@ -56,7 +56,7 @@
     }
 
     function submitForm(formData) {
-      fetch("https://fervent-thompson-1b8733.netlify.app/thankyou", {
+      fetch("https://fervent-thompson-1b8733.netlify.app/", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: encode(formData),
