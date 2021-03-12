3e2dc0c5c2096ec2c6d11a755e7f5be7e3a5f547
JackRyan1989 <jack.jackryan@gmail.com>
2021-03-11 20:38:02 -0500

    Fixing form submit destination.

--- src/components/contact-form.svelte
+++ src/components/contact-form.svelte
@@ -114,7 +114,6 @@
       netlify-honeypot="botfield"
       class="uk-form-horizontal uk-margin-medium"
       method="POST"
-      action="/thankyou/"
     >
       <div class="uk-margin">
         <label class="uk-form-label" for="name">Full Name</label>
