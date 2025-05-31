d6579cdf3cfa1f888be3d84cc9ec8710010df038
JackRyan1989 <jack.jackryan@protonmail.com>
2025-05-31 00:20:10 -0400

    Now using tailwind and how

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,26 @@
-import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+import fs from 'fs';
+import path from 'path';
+import postcss from 'postcss';
+import tailwindcss from '@tailwindcss/postcss';
 
 export default async function(eleventyConfig) {
-	eleventyConfig.addPassthroughCopy("assets/images");
-    eleventyConfig.addPassthroughCopy("assets/fonts");
-    eleventyConfig.addPassthroughCopy("styles.css");
+    eleventyConfig.on('eleventy.before', async () => {
+        const tailwindInputPath = path.resolve('./styles.css');
+        const tailwindOutputPath = './_site/styles.css';
+        const cssContent = fs.readFileSync(tailwindInputPath, 'utf8');
+        const outputDir = path.dirname(tailwindOutputPath);
 
-    eleventyConfig.addPlugin(eleventyImageTransformPlugin);
+        if (!fs.existsSync(outputDir)) {
+          fs.mkdirSync(outputDir, { recursive: true });
+        }
+
+        const result = await postcss([tailwindcss()]).process(cssContent, {
+          from: tailwindInputPath,
+          to: tailwindOutputPath,
+        });
+
+        fs.writeFileSync(tailwindOutputPath, result.css);
+      });
+      eleventyConfig.addPassthroughCopy("assets/images");
+      eleventyConfig.addPassthroughCopy("assets/fonts");
 };
