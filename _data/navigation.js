4e36222b3a6a1e4903230b6814ac5554bd87cc4e
JackRyan1989 <jack.jackryan@protonmail.com>
2025-08-23 22:45:44 -0400

    Fix gitignore and get files to consume data

--- _data/navigation.js
+++
@@ -1,7 +0,0 @@
-export default {
-    urls: [
-        {endpoint: '/approach', text: 'Approach'},
-        {endpoint: '/services', text: 'Services'},
-        {endpoint: '/contact', test: 'Contact'},
-    ]
-}
--- eleventy.config.js
+++ eleventy.config.js
@@ -30,7 +30,6 @@ export const config = {
 	dir: {
 		input: "content",
 		includes: "../_includes",
-		data: "../_data",
 		output: "_site"
 	}
 }
